87de94032f03ee81d42f437a0eae2b0d1c169a14
sqrtqiezi <njin.cool@gmail.com>
2018-02-09 23:25:46 +0800

    feat: 完成界面样式和部分交互

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
 //app.js
+const AV = require("./libs/av-weapp-min.js");
+
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch: function() {
+    // 初始化 LeanCloud
+    AV.init({
+      appId: "RcQJijiiVGbsuG52OGwDrPBS-9Nh9j0Va",
+      appKey: "qxjVTNIR2HG2BUE8fgaSrap4"
+    });
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+    this.globalData.AV = AV;
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    AV: null
   }
-})
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,114 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    index: -1,
+    songs: [],
+    icons: {
+      play: "../../assets/images/music_play_button.png",
+      pause: "../../assets/images/music_pause_button.png",
+      playlist: "../../assets/images/music_playlist.png",
+      forward: "../../assets/images/music_fastforward_button.png",
+      backward: "../../assets/images/music_rewind_button.png",
+      repeat: "../../assets/images/music_repeat_button.png",
+      shuffle: "../../assets/images/music_shuffle_button.png"
+    },
+    control: {
+      isPlaying: false,
+      isRepeat: true
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  onLoad() {
+    this.setNavigationBarTitle();
+  },
+
+  onReady() {
+    this.loadData();
+  },
+
+  play() {
+    this.setData({ "control.isPlaying": !this.data.control.isPlaying });
+  },
+
+  forward() {
+    const index = this.data.index + 1;
+    if (index < this.data.songs.length) {
+      this.setData({ index }, this.setNavigationBarTitle.bind(this));
+    } else {
+      this.setData({ index: 0 }, this.setNavigationBarTitle.bind(this));
+    }
+  },
+
+  backward() {
+    const index = this.data.index - 1;
+    if (index < 0) {
+      this.setData(
+        { index: this.data.songs.length - 1 },
+        this.setNavigationBarTitle.bind(this)
+      );
+    } else {
+      this.setData({ index }, this.setNavigationBarTitle.bind(this));
+    }
+  },
+
+  changeMode() {
+    this.setData({ "control.isRepeat": !this.data.control.isRepeat });
+  },
+
+  showPlaylist() {
+    const songs = this.data.songs.map(item => {
+      return item.title + " - " + item.artist;
+    });
+
+    wx.showActionSheet({
+      itemList: songs,
+      success: res => {
+        this.setData({ index: res.tapIndex });
+      }
+    });
+  },
+
+  loadData() {
+    wx.showLoading({ title: "加载中..." });
+
+    new app.globalData.AV.Query("Song")
+      .limit(6) // 坑爹的小程序 showActionSheet 只支持六个选项
+      .find()
+      .then(results => {
+        if (results.length > 0) {
+          const songs = [];
+          results.forEach(item => {
+            songs.push({
+              title: item.attributes.title,
+              artist: item.attributes.artist,
+              cover: item.attributes.cover.attributes.url,
+              music_url: item.attributes.music_data.attributes.url
+            });
+          });
+
+          this.setData(
+            {
+              songs,
+              index: 0
+            },
+            () => {
+              this.setNavigationBarTitle();
+              wx.hideLoading();
+            }
+          );
+        }
+      });
+  },
+
+  setNavigationBarTitle() {
+    const index = this.data.index;
+    if (index >= 0) {
+      const song = this.data.songs[index];
+      const title = `${song.title} - ${song.artist}`;
+      wx.setNavigationBarTitle({ title });
+    }
   }
-})
+});
